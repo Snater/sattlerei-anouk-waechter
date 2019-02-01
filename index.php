ea69a83787454f0576059eae0e55bf9724924bde
Snater <git@snater.com>
2019-02-01 15:22:14 +0100

    Removed obsolete variables

--- index.php
+++ index.php
@@ -7,11 +7,7 @@ require_once __DIR__
 $loader = new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . 'templates');
 $twig = new Twig_Environment($loader);
 
-$REMOTE_DIR = './';
-
 function getImages($subDir) {
-	global $REMOTE_DIR;
-
 	$files = [];
 	$dir = __DIR__ . DIRECTORY_SEPARATOR . 'img' . DIRECTORY_SEPARATOR . $subDir;
 
@@ -20,19 +16,16 @@ function getImages($subDir) {
 	}
 
 	foreach (array_diff(scandir($dir), ['.', '..']) as $filename) {
-		$files[] = $REMOTE_DIR . 'img/' . $subDir . '/' . $filename;
+		$files[] = 'img/' . $subDir . '/' . $filename;
 	}
 
 	return $files;
 }
-
-$TEMPLATE_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
 
 $exe = isset($_GET['exe']) ? $_GET['exe'] : 'index';
 
 echo $twig->render($exe . '.html.twig', [
 	'exe' => $exe,
 	'is_front' => $exe === 'index',
-	'REMOTE_DIR' => $REMOTE_DIR,
 	'images' => getImages($exe),
 ]);
