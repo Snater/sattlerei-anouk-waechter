c25abfb89a5575e35a7b146e58e02bf5613f3636
Snater <git@snater.com>
2019-02-01 15:13:37 +0100

    Conversion to twig templates

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
+require_once __DIR__
+	. DIRECTORY_SEPARATOR . 'vendor'
+	. DIRECTORY_SEPARATOR . 'autoload.php';
+
+$loader = new Twig_Loader_Filesystem(__DIR__ . DIRECTORY_SEPARATOR . 'templates');
+$twig = new Twig_Environment($loader);
+
 $REMOTE_DIR = './';
 
 function getImages($subDir) {
 	global $REMOTE_DIR;
 
 	$files = [];
+	$dir = __DIR__ . DIRECTORY_SEPARATOR . 'img' . DIRECTORY_SEPARATOR . $subDir;
 
-	foreach (array_diff(scandir(__DIR__ . DIRECTORY_SEPARATOR . 'img' . DIRECTORY_SEPARATOR . $subDir), ['.', '..']) as $filename) {
+	if (!file_exists($dir)) {
+		return $files;
+	}
+
+	foreach (array_diff(scandir($dir), ['.', '..']) as $filename) {
 		$files[] = $REMOTE_DIR . 'img/' . $subDir . '/' . $filename;
 	}
 
@@ -16,6 +28,11 @@ function getImages($subDir) {
 
 $TEMPLATE_DIR = __DIR__ . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR;
 
-$exe = isset($_GET['exe']) ? $_GET['exe'] : null;
+$exe = isset($_GET['exe']) ? $_GET['exe'] : 'index';
 
-include $TEMPLATE_DIR . 'html.php';
+echo $twig->render('html.html.twig', [
+	'exe' => $exe,
+	'is_front' => $exe === 'index',
+	'REMOTE_DIR' => $REMOTE_DIR,
+	'images' => getImages($exe),
+]);
